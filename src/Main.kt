f84065f1bcbd737b01f0186f4f375780a779740f
jhonatanbrum-xmart <jhonatan.brum@xmartlabs.com>
2026-06-04 17:59:38 -0300

    ejercicios del 1 al 7, se me olvido ponerlos 1 por uno jajajaj

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,8 @@
+import java.util.Locale
+import java.util.Locale.getDefault
+
 fun main() {
+    /* EJERCICIO 1
     val year: Int = 1234
     val longer: Long = 2000000000000
     val flotant: Float = 3.14F
@@ -8,6 +12,38 @@ fun main() {
     val names = arrayOf("Eliza", "Miguel", "Juan")
     val numbers = listOf("One", "Two", "Three")
     val mutantNumbers = mutableListOf("One", "Two", "Three")
-    println("\$year $year $longer $flotant $bool $character $text $names ${numbers} $mutantNumbers")
-    println("$year $longer $flotant")
+    println("$year $longer $flotant $bool $character $text ${names.contentToString()} ${numbers} $mutantNumbers")
+    println("$year $longer $flotant")*/
+    /* EJERCICIO 2
+    var num1 = 10
+    var num2 = 4
+    var difference = num1 + num2
+    var sum = num1 - num2
+    var product = num1 % num2
+    println("$difference, $sum, $product")*/
+    /* EJERCICIO 3
+    val me = "Jhonatan"
+    println("${me.length}, ${me.uppercase(getDefault())}")*/
+    /* EJERCICIO 4
+    val vof = !true
+    println(vof)*/
+    /* EJERCICIO 5
+    val numbersArraysCreatedForMe = arrayOf(1,2,3,4,5)
+    println("${numbersArraysCreatedForMe.first()}, ${numbersArraysCreatedForMe.last()}")*/
+    /* EJERCICIO 6
+    val favoriteApps = listOf("IntelliJ IDEA", "Android Studio", "WhatsApp", "TikTok", "Discord")
+    val randomApp = "TikTok"
+    if (favoriteApps.contains(randomApp)) {
+        println("La app: $randomApp si esta dentro de las apps favoritas.")
+    } else {
+        println("La app: $randomApp no esta dentro de las apps favoritas.")
+    }*/
+    /* EJERCICIO 7
+    var mutableListCreatedForMe = mutableSetOf<String>()
+    mutableListCreatedForMe.add("Mutable")
+    mutableListCreatedForMe.add("List")
+    mutableListCreatedForMe.add("Created for me")
+    mutableListCreatedForMe.remove("Created for me")
+    println(mutableListCreatedForMe)*/
+
 }
