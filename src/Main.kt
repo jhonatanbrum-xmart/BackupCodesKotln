1201f2f750dd1e03b801a3096ffb6a84a31cfe41
jhonatanbrum-xmart <jhonatan.brum@xmartlabs.com>
2026-06-11 15:59:27 -0300

    Exercise 1 completed

--- src/Main.kt
+++ src/Main.kt
@@ -9,7 +9,52 @@ fun ejercicio1() {
     val text: String = "Hola"
     val names = arrayOf("Eliza", "Miguel", "Juan")
     val numbers = listOf("One", "Two", "Three")
-    val mutantNumbers = mutableListOf("One", "Two", "Three")
+    val mutantNumbers = mutableListOf("One", "Two", "Three", "four")
+    year.minus(12)
+    year.plus(1)
+    year.toString()
+    println(year.toFloat()-1.1) //Permite agregarle un valor "flotante" al momento de escribir un tipo Int
+    println(year.toChar()) //Convierte el numero entero al equivalente en simbolo(caracter).
+    longer.compareTo(year)
+    longer.toString()
+    longer.plus(1)
+    Long.MIN_VALUE //Permite conocer el minimo valor posible de un tipo Long
+    longer.toFloat() //Permite agregarle un valor "flotante" al moemnto de escribir o usar un tipo Long
+    flotant.toString()
+    flotant.plus(1)
+    flotant.times(1)
+    flotant.isFinite() //Devuelve true si el valor esta dentro de los numeros que no son ni NaN ni infinito
+    flotant.isInfinite() //Devuelve true si el falor es infinito, sea positivo o negativo
+    bool.not()
+    bool.compareTo(false)
+    bool.toString()
+    bool.or(bool) //Devuelve verdadero si al menos uno es verdadero
+    bool.and(bool) //Devuelve verdadero si los 2 son verdaderos, en otro caso dara falso
+    character.isLetter()
+    character.isDigit()
+    character.uppercaseChar()
+    character.lowercaseChar() //Devuelve el caracter en minuscula en caso de ser una letra, si no devuelve el mismo  tal cual
+    character.isLetterOrDigit() //Infoma si el numero proveido es una letra o es un digito.
+    text.length
+    text.uppercase()
+    text.substring(0, 1)
+    text.get(1) //Obtiene el caracter(sea letra, digito, numero, caracter especial, etc) en la posicion que se especifica
+    text.startsWith("a") //Comprueba si el texto comienza con ese caracter o no.
+    names.size
+    names.get(1)
+    names.set(1, "Pepe")
+    names.first() //Obtiene el primer elemento del array
+    names.last() //Obtiene el ultimo elemento del array
+    numbers.size
+    numbers.contains("one")
+    numbers.forEach { number -> println(number) }
+    numbers.firstOrNull() //Obtiene el primer elemento de la lista o devuelve Null si la lista esta vacia
+    numbers.lastOrNull() //Obtiene el ultimo elemento del lista o devuelve Null si la lista esta vacia
+    mutantNumbers.add("four")
+    mutantNumbers.remove("four")
+    mutantNumbers.clear()
+    mutantNumbers.removeAt(2) //Remueve el elemento que este en esa casilla de la lista
+    mutantNumbers.removeIf { number ->  number == "Three" }  //Remueve los elementos que cumplan con las condiciones del if
     println("$year $longer $flotant $bool $character $text ${names.contentToString()} ${numbers} $mutantNumbers")
     println("$year $longer $flotant")
 }
@@ -59,7 +104,9 @@ fun ejercicio7() {
 
 fun ejercicio8() {
     val hooray = '·'
-    when (hooray) {}
+    when (hooray) {
+
+    }
     if (hooray.isLetter()) {
         println("Hooray")
     } else if (hooray.isDigit()) {
@@ -73,6 +120,7 @@ fun ejercicio9() {
     val numero = 0
     println(numero.toString() + 123)
 }
+
 fun main() {
-    ejercicio7()
+    ejercicio1()
 }
