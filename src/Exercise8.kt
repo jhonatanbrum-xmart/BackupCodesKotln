a7f4acb80bae6c64e6befba7c80e2910feed565a
jhonatanbrum-xmart <jhonatan.brum@xmartlabs.com>
2026-06-16 16:02:59 -0300

    Exercise 8 with "when"

--- src/Exercise8.kt
+++ src/Exercise8.kt
@@ -1,13 +1,14 @@
 fun exercise8() {
     val hooray = '·'
-    when (hooray) {
-
-    }
-    if (hooray.isLetter()) {
-        println("Hooray")
-    } else if (hooray.isDigit()) {
-        println("Hooray?")
-    } else {
-        println("Bruh, it was a letter or a digit, not a letter, digit or a random character of your mind.")
+    when {
+        hooray.isLetter() -> {
+            println("Hooray")
+        }
+        hooray.isDigit() -> {
+            println("Hooray?")
+        }
+        else -> {
+            println("Bruh, it was a letter or a digit, not a letter, digit or a random character of your mind.")
+        }
     }
 }
