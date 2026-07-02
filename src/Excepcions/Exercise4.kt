78aee7c455ea1e394b31c39d600160ff3662f434
jhonatanbrum-xmart <jhonatan.brum@xmartlabs.com>
2026-07-02 17:24:10 -0300

    Exercise 4 of exceptions.

---
+++ src/Excepcions/Exercise4.kt
@@ -0,0 +1,11 @@
+package Excepcions
+
+fun main(){
+    val age: Int = try {
+        "25".toInt()
+    } catch(e: NumberFormatException){
+        -1
+    }
+    println(age)
+}
+//The expected output will "25"
