7f773d9ddfca983b2ec27f237b1bebece1eba1ca
jhonatanbrum-xmart <jhonatan.brum@xmartlabs.com>
2026-07-02 16:23:31 -0300

    Without "println(number1)"

--- src/Excepcions/ExampleRunCatching.kt
+++ src/Excepcions/ExampleRunCatching.kt
@@ -8,6 +8,5 @@ fun main() {
         .onFailure { println(0) }
 
     println(number)
-    println(number1)
     }
 //The expected result of this code is a "0" and a text saying "The process end, converted: 123".
