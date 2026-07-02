20f9a8e1e6629eaeabdbedb3356363f89efbf4e1
jhonatanbrum-xmart <jhonatan.brum@xmartlabs.com>
2026-07-02 17:01:21 -0300

    Exercise 2 of exceptions.

---
+++ src/Excepcions/Exercise2.kt
@@ -0,0 +1,15 @@
+package Excepcions
+
+fun divide(a: Int, b: Int): Int {
+    val asd = a / b
+    return asd
+}
+fun main(){
+    val test1 = try {
+        divide(1, 0)
+    } catch(_: ArithmeticException){
+        println("Cannot divide by zero.")
+    }
+    println(test1)
+}
+// The expected result is a text saying "Cannot divide by zero."
