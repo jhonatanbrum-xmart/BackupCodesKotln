322373f2b829694200081036dd225e670f9b5bf4
jhonatanbrum-xmart <jhonatan.brum@xmartlabs.com>
2026-07-07 16:27:25 -0300

    Exercise 7 completed

--- src/exceptions/Exercise7.kt
+++ src/exceptions/Exercise7.kt
@@ -3,18 +3,19 @@ package exceptions
 private class BankAccount(val open: Boolean, var money: Int) {
     fun withdraw(amount: Int) {
         check(open)
+        check(money>=amount)
         money -= amount
     }
 }
 
-fun main(){
+fun main() {
     val account = BankAccount(true, 100)
-    check((account.money>=100))
     account.withdraw(100)
     println(account.money)
-    val account2 = BankAccount(false, 100)
-    check((account2.money >= 100))
-    account2.withdraw(100)
-    println(account2.money)
+    try{val account2 = BankAccount(false, 99)
+        account2.withdraw(100)
+        println(account2.money)
+    }catch (e: IllegalStateException){
+            println("You dont have sufficient amount of money to the withdraw or you dont have an opened account.")}
 }
-// The expected output will a number "0" and a IllegalStateException
+// The expected output will a number "0" and a text saying "You dont have sufficient amount of money to the withdraw or you dont have an opened account."
