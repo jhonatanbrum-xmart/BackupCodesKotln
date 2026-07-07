98ad48808fde52563b17454ddf91fe7f98e610ef
jhonatanbrum-xmart <jhonatan.brum@xmartlabs.com>
2026-07-07 14:41:11 -0300

    Exercise 7

---
+++ src/Excepcions/Exercise7.kt
@@ -0,0 +1,20 @@
+package Excepcions
+
+class BankAccount(val open: Boolean, var money: Int) {
+    fun withdraw(amount: Int) {
+        check(open)
+        money = money-amount
+    }
+}
+
+fun main(){
+    val account = BankAccount(true, 100)
+    check((account.money>=100))
+    account.withdraw(100)
+    println(account.money)
+    val account2 = BankAccount(false, 100)
+    check((account2.money >= 100))
+    account2.withdraw(100)
+    println(account2.money)
+}
+// The expected output will a number "0" and a IllegalStateException
