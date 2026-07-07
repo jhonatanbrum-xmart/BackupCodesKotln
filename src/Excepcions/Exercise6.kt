d34385df7d1b816862fa9796333b6ffa42f7c8fb
jhonatanbrum-xmart <jhonatan.brum@xmartlabs.com>
2026-07-07 15:40:24 -0300

    Exercise 6 complete

---
+++ src/Excepcions/Exercise6.kt
@@ -0,0 +1,18 @@
+package Excepcions
+
+fun createUser(name: String){
+    require(name.isNotEmpty())
+    println(name)
+}
+fun main(){
+    try{
+    createUser("")
+}catch (e: IllegalArgumentException){
+    println("The name is not valid")
+}
+    try {
+    createUser("John")
+}catch (e: IllegalArgumentException){
+    println("The name is not valid")
+}}
+//The expected output is a text saying "The name is not valid" and a text saying "John"
