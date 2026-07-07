ad599f9b6840ca7d2a754478d7643ddaa45083c1
jhonatanbrum-xmart <jhonatan.brum@xmartlabs.com>
2026-07-07 15:36:26 -0300

    Exercise 8

---
+++ src/Excepcions/Exercise8.kt
@@ -0,0 +1,11 @@
+package Excepcions
+
+fun main(){
+    val name: String? = null
+    try{println(name!!)
+    }catch (e: NullPointerException){
+        println("Unknown user")
+    }
+    println(if(name?.length == null){"Unknown user"}else{name})
+    println(name ?: "Unknown user")
+}
