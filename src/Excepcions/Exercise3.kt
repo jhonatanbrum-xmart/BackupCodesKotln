9b868995af898aa2eb05a777adb89be23fa61ba8
jhonatanbrum-xmart <jhonatan.brum@xmartlabs.com>
2026-07-07 15:32:00 -0300

    Exercise 3 complete

--- src/Excepcions/Exercise3.kt
+++ src/Excepcions/Exercise3.kt
@@ -4,6 +4,7 @@ fun main(){
     try {
         println("Opening file...")
         val a = 1/0
+        println("End of try")
     } catch (_: ArithmeticException){
         println("")
     }finally {
